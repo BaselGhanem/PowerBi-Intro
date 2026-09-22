4b5d21e20a0052a1f10786fd8c2b86fa823bca39
Basel Ghanem <baselghanem21@gmail.com>
2026-09-22 22:40:12 +0300

    Add iframe code for video on day 2

--- videos.js
+++ videos.js
@@ -3,7 +3,7 @@
 
 const DAY_VIDEOS = {
   1: `<iframe src="https://1drv.ms/v/c/a1a27fad04d192e9/IQQ1ap9rXHqTRIOXnbs6dHSyAU4SDPKYweIgT7t9-M3ZI-M" width="1920" height="1080" frameborder="0" scrolling="no" allowfullscreen></iframe>`,
-  2: ``,
+  2: `<iframe src="https://1drv.ms/v/c/a1a27fad04d192e9/IQQdzObmVlepT7WOdE4uwMxvAYXzc2tIlFNgx_WoGPSjqpk" width="1920" height="1080" frameborder="0" scrolling="no" allowfullscreen></iframe>`,
   3: ``,
   4: ``,
   5: ``,
